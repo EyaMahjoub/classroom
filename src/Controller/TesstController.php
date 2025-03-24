17ba51f746bf6ed8816ed6f49e3239160e4d9a34
EyaMahjoub <mahjouba562@gmail.com>
2025-03-24 23:15:28 +0100

    2 commit

---
+++ src/Controller/TesstController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class TesstController extends AbstractController
+{
+    #[Route('/tesst', name: 'app_tesst')]
+    public function index(): Response
+    {
+        return $this->render('tesst/index.html.twig', ['salut'=>"saluttttt" 
+        ]);
+    }
+}
